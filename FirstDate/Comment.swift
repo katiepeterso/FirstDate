09831d2a71632259eea82fe705f3d32022146ede
Alp Eren Can <alperenc@gmail.com>
2015-09-22 16:00:51 -0700

    change Comment to a struct written in Swift

---
+++ FirstDate/Comment.swift
@@ -0,0 +1,17 @@
+//
+//  Comment.swift
+//  FirstDate
+//
+//  Created by Alp Eren Can on 22/09/15.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+import UIKit
+
+struct Comment {
+    
+    let user: User
+    let content: String
+    let timeStamp: NSDate
+    
+}
