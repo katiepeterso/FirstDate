2d65050664d02b4a00a43fd271411f2bbbda1e5f
katiepeterso <katiepeterso@outlook.com>
2015-09-25 11:22:54 -0700

    add collection view cell to user profile

---
+++ FirstDate/UserProfileDateIdeasCell.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileDateIdeasCell.h
+//  FirstDate
+//
+//  Created by Katherine Peterson on 2015-09-25.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileDateIdeasCell : UICollectionViewCell
+
+@end
---
+++ FirstDate/UserProfileDateIdeasCell.m
@@ -0,0 +1,20 @@
+//
+//  UserProfileDateIdeasCell.m
+//  FirstDate
+//
+//  Created by Katherine Peterson on 2015-09-25.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "UserProfileDateIdeasCell.h"
+
+@interface UserProfileDateIdeasCell ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *dateIdeaImageView;
+@property (weak, nonatomic) IBOutlet UIVisualEffectView *dateIdeaEffectView;
+
+@end
+
+@implementation UserProfileDateIdeasCell
+
+@end
