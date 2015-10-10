1a6d0598f12f066b8925b7b388e0bf00dc4e5ef7
katiepeterso <katiepeterso@outlook.com>
2015-10-10 14:07:06 -0700

    setup Parse and persist messages

--- FirstDate/Message.swift
+++ FirstDate/Message.swift
@@ -2,52 +2,12 @@
 //  Message.swift
 //  FirstDate
 //
-//  Created by Katherine Peterson on 2015-10-09.
+//  Created by Katherine Peterson on 2015-10-10.
 //  Copyright © 2015 KatieExpatriated. All rights reserved.
 //
 
 import UIKit
-import Parse
-import JSQMessagesViewController
-import Foundation
 
-class Message: PFObject, JSQMessageData {
-    @NSManaged var sendingUser: User
-    @NSManaged var messagingHash: NSNumber
-    @NSManaged var messagingText: NSString
-    @NSManaged var receivingUser: User
-    
-    convenience init(text: String?, sender: User?, receiver: User?) {
-        self.init()
-        self.messagingText = text!
-        self.sendingUser = sender!
-        self.messagingHash = NSNumber(integer: (objectId?.hash)!)
-        self.receivingUser = receiver!
-        
-    }
-    
-    func senderId() -> String! {
-        return sendingUser.objectId
-    }
-    
-    func senderDisplayName() -> String! {
-        return sendingUser.username
-    }
-    
-    func date() -> NSDate! {
-        return self.createdAt
-    }
-    
-    func isMediaMessage() -> Bool {
-        return false
-    }
-    
-    func messageHash() -> UInt {
-        return messagingHash.unsignedLongValue
-    }
-    
-    func text() -> String! {
-        return messagingText as String
-    }
-    
+class Message: PFObject {
+
 }
--- FirstDate/MessagingViewController.swift
+++ FirstDate/MessagingViewController.swift
@@ -2,64 +2,34 @@
 //  MessagingViewController.swift
 //  FirstDate
 //
-//  Created by Katherine Peterson on 2015-10-08.
+//  Created by Katherine Peterson on 2015-10-10.
 //  Copyright © 2015 KatieExpatriated. All rights reserved.
 //
 
 import UIKit
-import Parse
-import JSQMessagesViewController
 
 class MessagingViewController: JSQMessagesViewController {
-    
-    var messages = [Message]()
-    
-    let bubbleFactory = JSQMessagesBubbleImageFactory()
-    var senderPhoto = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if (User.currentUser() != nil) {
-            self.senderDisplayName = User.currentUser()!.username
-            senderId = User.currentUser()!.objectId
-        }
-        
-        fetchConversation()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    func fetchConversation() {
-        let getMessages = PFQuery(className:"Message")
-        getMessages.whereKey("sender", equalTo:User.currentUser()!)
-        getMessages.includeKey("sender")
-        getMessages.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
-            if(results!.count != 0) {
-                self.messages += results as! [Message]
-            }
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-        
-        
-    //MARK: JSQMessagesCollectionViewDataSource
-    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
-        return self.messages[indexPath.item]
-    }
-    
-    override func collectionView(collectionView: JSQMessagesCollectionView!, didDeleteMessageAtIndexPath indexPath: NSIndexPath!) {
-        self.messages.removeAtIndex(indexPath.item)
-    }
-    
-    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
-        if(messages[indexPath.item].sendingUser == User.currentUser()) {
-            return bubbleFactory.outgoingMessagesBubbleImageWithColor(UIColor (red: 80.0/255.0, green: 210.0/255.0, blue: 194.0/255.0, alpha: 1.0))
-        } else {
-            return bubbleFactory.incomingMessagesBubbleImageWithColor(UIColor (red: 158.0/255.0, green: 37.0/255.0, blue: 143.0/255.0, alpha: 1.0))
-        }
-    }
-    
-    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
-        PhotoHelper.getPhotoInBackground(self.messages[indexPath.item].sendingUser.userPhoto) { (resultImage) -> Void in
-            self.senderPhoto = resultImage!
-        }
-        return JSQMessagesAvatarImageFactory.avatarImageWithImage(self.senderPhoto, diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
-    }
+    */
+
 }
