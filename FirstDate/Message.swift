28139aac1e2aee6158d3b9db8b35c3b1fb917d4e
katiepeterso <katiepeterso@outlook.com>
2015-10-10 14:07:56 -0700

    setup Parse and persist messages

--- FirstDate/Message.swift
+++ FirstDate/Message.swift
@@ -7,7 +7,66 @@
 //
 
 import UIKit
+import Parse
+import JSQMessagesViewController
+import Foundation
 
-class Message: PFObject {
+class Message: PFObject, PFSubclassing, JSQMessageData {
+    @NSManaged var sendingUser: User
+    @NSManaged var messagingHash: NSNumber
+    @NSManaged var messagingText: NSString
+    @NSManaged var receivingUser: User
+    
+    override class func initialize() {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0;
+        }
+        dispatch_once(&Static.onceToken) {
+            self.registerSubclass()
+        }
+    }
+    
+    init(text: String, sender: User, receiver: User) {
+        super.init()
+        self.messagingText = text
+        self.sendingUser = sender
+        let now = NSDate()
+        self.messagingHash = NSNumber(integer: (self.sendingUser.objectId!.hash ^ now.hash ^ self.messagingText.hash))
+        self.receivingUser = receiver
+        
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    static func parseClassName() -> String {
+        return "Message"
+    }
 
+    
+    func senderId() -> String! {
+        return sendingUser.objectId
+    }
+    
+    func senderDisplayName() -> String! {
+        return sendingUser.username
+    }
+    
+    func date() -> NSDate! {
+        return self.createdAt
+    }
+    
+    func isMediaMessage() -> Bool {
+        return false
+    }
+    
+    func messageHash() -> UInt {
+        return messagingHash.unsignedLongValue
+    }
+    
+    func text() -> String! {
+        return messagingText as String
+    }
+    
 }
