b22549ad67cbb8bbdee5f5df43747bf823f863c6
katiepeterso <katiepeterso@outlook.com>
2015-10-12 17:40:25 -0700

    added custom cell and tableview to handle selecting a user message

---
+++ FirstDate/DateIdeaHeartedByCell.swift
@@ -0,0 +1,27 @@
+//
+//  DateIdeaHeartedByCell.swift
+//  FirstDate
+//
+//  Created by Katherine Peterson on 2015-10-12.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+import UIKit
+
+class DateIdeaHeartedByCell: UITableViewCell {
+
+    @IBOutlet weak var userPhotoImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
